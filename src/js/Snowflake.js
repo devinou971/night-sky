d821dda6e8054262c26aafab9d02b8053528e85f
Devinou971 <damiendasseux@gmail.com>
2022-01-17 15:21:17 +0100

    Snow added

--- src/js/Snowflake.js
+++ src/js/Snowflake.js
@@ -1,8 +1,37 @@
 class Swnoflake{
-    constructor(x, y, depth){
+    constructor(x, y, radius, alpha, speed, ctx){
         this.x = x
         this.y = y
-        this.depth
+        this.radius = radius
+        this.color = [255, 255, 255]
+        this.alpha = alpha
+        this.speed = speed
+        this.ctx = ctx
+    }
+    
+    move(angle){
+        if (this.y > window.innerHeight){
+            this.y = -10
+            this.x = window.innerWidth * Math.random()
+        }
+        
+        if(this.x < -10){
+            this.x = window.innerWidth
+        }
+
+        else if(this.x > window.innerWidth + this.radius){
+            this.x = -10
+        }
+
+        this.y += (this.speed * Math.sin(angle)) 
+        this.x += (this.speed * Math.cos(angle))
+    }
+
+    draw(){
+        this.ctx.fillStyle = 'rgba(' + this.color[0].toString() + ", " + this.color[1].toString() + ", " + this.color[2].toString() + ", " + this.alpha + ")" 
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI)
+        this.ctx.fill()
     }
     
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,123 @@
-/*
-let c = document.querySelector("#canvas")
-let ctx = c.getContext("2d")
+const FPS = 20;
+//const FPS = 0.1;
 
-ctx.fillStyle = 'rgb(200, 0, 0)';
-ctx.fillRect(10, 10, 50, 50);
 
-ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-ctx.fillRect(30, 30, 50, 50);
-*/
+for(let o of document.querySelectorAll("canvas")){
+    o.style.width = window.innerWidth.toString() + "px"
+    o.style.height = window.innerHeight.toString() + "px"
+    o.width = window.innerWidth
+    o.height = window.innerHeight
+}
 
-window.onload = ()=>{
-    for(let o of document.querySelectorAll("canvas")){
-        o.style.width = window.innerWidth.toString() + "px"
-        o.style.height = window.innerHeight.toString() + "px"
-        console.log("")
-    }
+let canvas = document.querySelectorAll(".drawingCanvas")
+let ctx = []
+for (let c = 0; c<canvas.length; c++){
+    ctx[c] = canvas[c].getContext("2d")
+}
+
+snowflakes0 = []
+for(let i=0; i < 50; i++){
+    x_position = Math.random()*window.innerWidth
+    y_position = Math.random() * window.innerHeight - 10
+    radius = 2*Math.random()
+    alpha = Math.random() * 0.5
+    speed = Math.random() *0.5
+
+    snowflakes0[i] = new Swnoflake(x_position, y_position, radius, alpha, speed, ctx[0])
+}
+
+snowflakes1 = Array(50)
+for(let i=0; i < snowflakes1.length; i++){
+    x_position = Math.random()*window.innerWidth
+    y_position = Math.random() * window.innerHeight - 10
+    radius = 2+2*Math.random()
+    alpha = Math.random() * 0.5
+    speed = 0.5 + Math.random() * 0.5
+
+    snowflakes1[i] = new Swnoflake(x_position, y_position, radius, alpha, speed, ctx[1])
+}
+
+snowflakes2 = Array(50)
+for(let i=0; i < snowflakes1.length; i++){
+    x_position = Math.random()*window.innerWidth
+    y_position = Math.random() * window.innerHeight - 10
+    radius = 2+2*Math.random()
+    alpha = 0.5 + Math.random() * 0.5
+    speed = 1 + Math.random(2)
+
+    snowflakes2[i] = new Swnoflake(x_position, y_position, radius, alpha, speed, ctx[2])
+}
+
+snowflakes3 = Array(50)
+for(let i=0; i < snowflakes1.length; i++){
+    x_position = Math.random()*window.innerWidth
+    y_position = Math.random() * window.innerHeight - 10
+    radius = 2+2*Math.random()
+    alpha = 0.5 + Math.random() * 0.5
+    speed = 1 + Math.random(2)
+
+    snowflakes3[i] = new Swnoflake(x_position, y_position, radius, alpha, speed, ctx[3])
+}
+
+snowflakes4 = Array(50)
+for(let i=0; i < snowflakes1.length; i++){
+    x_position = Math.random()*window.innerWidth
+    y_position = Math.random() * window.innerHeight - 10
+    radius = 2+2*Math.random()
+    alpha = 0.5 + Math.random() * 0.5
+    speed = 1 + Math.random(2)
+
+    snowflakes4[i] = new Swnoflake(x_position, y_position, radius, alpha, speed, ctx[4])
 }
 
 
 
+let angle = Math.PI/2
+
+document.addEventListener("mousemove", e=>{
+    x_mouse = e.clientX 
+    y_mouse = e.clientY
+
+    x_mouse = x_mouse - window.innerWidth/2
+
+    angle = Math.atan(y_mouse/x_mouse)
+    if(angle < 0){
+        angle = Math.PI + angle
+    }
+})
+
+
+
+
+setInterval(() => {
+    for(let c of ctx){
+        c.clearRect(0, 0, window.innerWidth, window.innerHeight)
+        c.clearRect(0, 0, 1000, 1000)
+    }
+
+    for(let snow of snowflakes0){
+        snow.move(angle)
+        snow.draw()
+    }
+
+    for(let snow of snowflakes1){
+        snow.move(angle)
+        snow.draw()
+    }
+
+    for(let snow of snowflakes2){
+        snow.move(angle)
+        snow.draw()
+    }
+
+    for(let snow of snowflakes3){
+        snow.move(angle)
+        snow.draw()
+    }
+
+    for(let snow of snowflakes4){
+        snow.move(angle)
+        snow.draw()
+    }
+    
+}, 100/FPS);
