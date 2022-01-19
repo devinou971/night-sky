26ea130a2dc558063bc2e13dcb9fadc31798e556
Devinou <damiendasseux@gmail.com>
2022-01-19 19:14:32 +0100

    added some functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,71 +1,53 @@
-const FPS = 20;
-
-window.onload = e=>{
-
+function setup_canvases(){
     for(let o of document.querySelectorAll("canvas")){
         o.style.width = window.innerWidth.toString() + "px"
         o.style.height = window.innerHeight.toString() + "px"
-    const FPS = 20;
-    o.width = window.innerWidth
+        o.width = window.innerWidth
         o.height = window.innerHeight
     }
-
-    let canvas = document.querySelectorAll(".drawingCanvas")
-    let ctx = []
-    for (let c = 0; c<canvas.length; c++){
-        ctx[c] = canvas[c].getContext("2d")
+}
+function drawAll(listSnowflakes, angle){
+    for(let o of listSnowflakes){
+        for(let snow of o){
+            snow.move(angle)
+            snow.draw()
+        }
     }
+}
 
-    snowflakes0 = []
-    for(let i=0; i < 100; i++){
+function createSnowflakes(number, min_speed, max_speed, min_alpha, max_alpha, min_radius, max_radius, ctx){
+    var arr = Array(number);
+    for (let i = 0; i < arr.length; i++) {
         x_position = Math.random() * window.innerWidth
         y_position = Math.random() * window.innerHeight - 10
-        radius = 2 * Math.random()
-        alpha = Math.random() * 0.5
-        speed = Math.random() * 0.25
-
-        snowflakes0[i] = new Swnoflake(x_position, y_position, radius, alpha, speed, ctx[0])
+        radius = min_radius + (max_radius-min_radius) * Math.random()
+        alpha = min_alpha +  (max_alpha-min_alpha) * Math.random() 
+        speed = min_speed + (max_speed-min_speed) * Math.random() * 0.25
+        arr[i] = new Swnoflake(x_position, y_position, radius, alpha, speed, ctx)
     }
+    return arr;
+}
 
-    snowflakes1 = Array(50)
-    for(let i=0; i < snowflakes1.length; i++){
-        x_position = Math.random() * window.innerWidth
-        y_position = Math.random() * window.innerHeight - 10
-        radius = 2+1*Math.random()
-        alpha = Math.random() * 0.5
-        speed = 0.2 + Math.random() * 0.5
-
-        snowflakes1[i] = new Swnoflake(x_position, y_position, radius, alpha, speed, ctx[1])
-    }
-
-    snowflakes2 = Array(50)
-    for(let i=0; i < snowflakes1.length; i++){
-        x_position = Math.random() * window.innerWidth
-        y_position = Math.random() * window.innerHeight - 10
-        radius = 2+2*Math.random()
-        alpha = 0.5 + Math.random() * 0.5
-        speed = 1 + Math.random(2)
-
-        snowflakes2[i] = new Swnoflake(x_position, y_position, radius, alpha, speed, ctx[2])
-    }
-
-    snowflakes3 = Array(50)
-    for(let i=0; i < snowflakes1.length; i++){
-        x_position = Math.random() * window.innerWidth
-        y_position = Math.random() * window.innerHeight - 10
-        radius = 3+2*Math.random()
-        alpha = 0.5 + Math.random() * 0.5
-        speed = 1 + Math.random(2)
-
-        snowflakes3[i] = new Swnoflake(x_position, y_position, radius, alpha, speed, ctx[3])
-    }
-
+window.onload = e=>{
+    const FPS = 20;
     let angle = Math.PI/2;
-    let speedMulti = 1;
     let burger = document.querySelector("#burger")
     let param = document.querySelector("#param")
     console.log(param)
     param.style.display = "none"
+    let canvas = document.querySelectorAll(".drawingCanvas")
+
+    setup_canvases()
+    
+    let ctxs = []
+    for (let c = 0; c<canvas.length; c++){
+        ctxs[c] = canvas[c].getContext("2d")
+    }
+
+    snowflakes0 = createSnowflakes(number=100, min_speed=0, max_speed=0.25, min_alpha=0.0, max_alpha=1, min_radius=0.0, max_radius=2,ctxs[0])
+    snowflakes1 = createSnowflakes(number=50, min_speed=0.2, max_speed=0.7, min_alpha=0.0, max_alpha=0.5, min_radius=1, max_radius=3,ctxs[1])
+    snowflakes2 = createSnowflakes(number=50, min_speed=1, max_speed=3, min_alpha=0.5, max_alpha=1, min_radius=2, max_radius=4,ctxs[2])
+    snowflakes3 = createSnowflakes(number=50, min_speed=1, max_speed=3, min_alpha=0.5, max_alpha=1, min_radius=3, max_radius=5,ctxs[3])
 
     burger.onclick = e => {
         let param = document.querySelector("#param")
@@ -101,30 +83,10 @@ window.onload = e=>{
     })
 
     setInterval(() => {
-        for(let c of ctx){
+        for(let c of ctxs){
             c.clearRect(0, 0, window.innerWidth, window.innerHeight)
             c.clearRect(0, 0, 1000, 1000)
         }
-
-        for(let snow of snowflakes0){
-            snow.move(angle)
-            snow.draw()
-        }
-
-        for(let snow of snowflakes1){
-            snow.move(angle)
-            snow.draw()
-        }
-
-        for(let snow of snowflakes2){
-            snow.move(angle)
-            snow.draw()
-        }
-
-        for(let snow of snowflakes3){
-            snow.move(angle)
-            snow.draw()
-        }
-
+        drawAll([snowflakes0, snowflakes1, snowflakes2, snowflakes3], angle)
     }, 100/FPS);
 }
