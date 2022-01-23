fe69046956dadee605a823d4019c73f054416e5e
Devinou <damiendasseux@gmail.com>
2022-01-23 10:21:58 +0100

    Adding more documentation and parameters

--- src/js/Snowflake.js
+++ src/js/Snowflake.js
@@ -1,21 +1,21 @@
 /**
- * Summary.
+ * @summary Creates a snowflake. It's used to draw snowflakes on the differents canvases
  * 
- * Description.
+ * @description Create a snowflake from it's position, radius, transparency and speed.
  * 
- * @file
- * @author
+ * @file Snowflake.js
+ * @author devinou971
  */
 class Swnoflake{
     /**
-     * 
+     * Gives the snowflake all his parameters
      * @class
-     * @param {*} x 
-     * @param {*} y 
-     * @param {*} radius 
-     * @param {*} alpha 
-     * @param {*} speed 
-     * @param {*} ctx 
+     * @param {Number} x The position of the snowflake on the x axis
+     * @param {Number} y The position of the snowflake on the y axis
+     * @param {Number} radius The radius of the circle that will represent the snwoflake
+     * @param {Number} alpha The transparency of the circle
+     * @param {Number} speed The movement speed of the circle.
+     * @param {CanvasRenderingContext2D} ctx The context the Snowflake will be drawn in
      */
     constructor(x, y, radius, alpha, speed, ctx){
         this.x = x
@@ -27,6 +27,11 @@ class Swnoflake{
         this.ctx = ctx
     }
     
+    /**
+     * 
+     * @param {Number} angle The angle the snowflake going be going towards. It's used to simulate wind 
+     * @param {Number} speedMulti A speed multiplier that can be set in the parameters in the UI.
+     */
     move(angle, speedMulti=1){
         if (this.y > window.innerHeight + this.radius){
             this.y = -10
@@ -45,6 +50,9 @@ class Swnoflake{
         this.x += (this.speed * speedMulti * Math.cos(angle))
     }
 
+    /**
+     * Draws the snowflake on the ctx.
+     */
     draw(){
         this.ctx.fillStyle = 'rgba(' + this.color[0].toString() + ", " + this.color[1].toString() + ", " + this.color[2].toString() + ", " + this.alpha + ")" 
         this.ctx.beginPath();
